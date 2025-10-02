efd0df9b58b166a05bcd3023212ed24536af1136
uremu-0 <cjoftherose@gmail.com>
2025-10-02 15:48:47 -0400

    resolved question 2

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 //code for problem 1
 
 const fiveToOneHundred = () => {
-//for loop to print 5-100
+    //for loop to print 5-100
     for (let i = 5; i <= 100; i++) {
         console.log(i);
     };
 };
 //fiveToOneHundred()
+// Write a function `multiplesOfThree` that console logs all positive numbers to 100 that is a multiple of 3.
+const multiplesOfThree = () => {
+    for (let i = 3; i <= 100; i += 3) {
+        // debugger
+        console.log(i)
+    };
+};
+// multiplesOfThree()
