057c2ec53912d0639e603d744433e33a87016d7c
Jonathan <JohnAnBer@gmail.com>
2025-10-02 16:25:17 -0400

    created a multiples of 3 or 5 func with comments

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ const multiplesOfThree = () => {
     };
 };
 // multiplesOfThree()
+
+
+//multiples of three or five up until 100
+const multiplesOfThreeOrFive = () => {
+// for loop that decides if a # is a multiple of 3 or 5
+    for (let i = 0; i <= 100; i++) {
+        // create an if else ... statement
+        if (i % 5 === 0) {
+            // multiples of 5
+            console.log(i)
+        } else if (i % 3 === 0) {
+            // mulitples of 3
+            console.log(i)
+        };
+    };
+};
+ multiplesOfThreeOrFive()
