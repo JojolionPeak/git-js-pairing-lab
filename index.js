b8bc34c71165dab14a875b57ef8b2a8e2b11daf3
Jonathan <JohnAnBer@gmail.com>
2025-10-02 15:29:52 -0400

    Solved first function question

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //Code your solutions in this file
+//code for problem 1
+const fiveToOneHundred = () => {
+//for loop to print 5-100
+    for (let i = 5; i <= 100; i++) {
+        console.log(i);
+    };
+};
+fiveToOneHundred()
