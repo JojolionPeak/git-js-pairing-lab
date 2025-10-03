c2f6c3314740faad66f203fa0c622fd5e793f3c6
uremu-0 <cjoftherose@gmail.com>
2025-10-02 22:24:11 -0400

    solved problem 10 shout out to the goat

--- index.js
+++ index.js
@@ -107,3 +107,17 @@ const betweenTwentyAndFourty = (n) => {
 }
 
 //console.log(betweenTwentyAndFourty(40))
+
+
+//Problem 10
+const largest = (a, b, c) => {
+    if (a > b && a > c) {
+        return a
+    } else if (b > a && b > c) {
+        return b
+    } else if (c > a && c > b) {
+        return c
+    }
+
+}
+console.log(largest(4, 6, 8))
